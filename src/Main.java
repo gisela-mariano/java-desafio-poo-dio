c4cdce659abb810cd32a277814e6b23321770bc7
Gisela Mariano <gisela-mariano@hotmail.com>
2025-08-05 11:41:24 -0300

    feat(class): add initial Mentoring and Course classes

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import br.com.dio.challenge.challenge.Course;
+
+public class Main {
+    public static void main(String[] args) {
+        Course course = new Course();
+
+        System.out.println(course);
+    }
+}
---
+++ src/br/com/dio/challenge/challenge/Mentoring.java
@@ -0,0 +1,42 @@
+package br.com.dio.challenge.challenge;
+
+import java.time.LocalDate;
+
+public class Mentoring {
+    String title;
+    String description;
+    LocalDate date;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + title + '\'' +
+                ", descricao='" + description + '\'' +
+                ", data=" + date +
+                '}';
+    }
+}
